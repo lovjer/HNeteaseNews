87716b7c17e58a7f30b1d24ae04af310eafa5718
lovjer <lovjer@163.com>
2016-03-13 21:30:45 +0800

    初始化项目

---
+++ 01-网易新闻/01-网易新闻/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  01-网易新闻
+//
+//  Created by lovjer on 16/3/13.
+//  Copyright © 2016年 lovjer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 01-网易新闻/01-网易新闻/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  01-网易新闻
+//
+//  Created by lovjer on 16/3/13.
+//  Copyright © 2016年 lovjer. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
